2dd0fef4ff7738925ed66c94fc7ddd1172fc30ae
styeee <91516623+styeee@users.noreply.github.com>
2021-09-28 13:42:34 +0700

    first edit
    add deferred generation, remove bitset (i dont use it), add send method (without descr)

--- signal/signal/signal.cpp
+++ signal/signal/signal.cpp
@@ -1,6 +1,5 @@
-﻿#include <iostream>
+#include <iostream>
 using namespace std;
-#include <bitset>
 
 class signal
 {
@@ -11,7 +10,7 @@ public:
     //generate new signal
     void generate(const size_t divisor)
     {
-         size_t i=0;
+         size_t i=0;//(*)
         for(size_t y=0;y<size;y++)//walk around bytes
         {
              self[y]=0;//clear byte from trash
@@ -19,16 +18,16 @@ public:
             {
                 if(i%divisor);else
                 self[y]|=1<<x;//set bit to true
-                i++;
+                i++;//(*)
             }
         }
     }
 
     //count - how many bytes // divisor - how many bits need to pass and -1
-    signal(const size_t count,const size_t divisor)    :size(count)
+    signal(const size_t count,size_t divisor=0)    :size(count)
     {
-        if(!divisor)exit(1);//dont make div with 0!
-        self=new char[count];//create new bytes massiv
+        self=new char[size];//create new bytes massiv
+        if(!divisor)return;//dont make div with 0! (but we can use it for make generation later)
         generate(divisor);//pull signal on it
     }
     ~signal()//if u want resize then delete and create new
@@ -44,25 +43,32 @@ public:
         }
     }
 
+    //send these bytes to some controller
+    const size_t send(const size_t how);
+
     const char*const raw()//just return bytes
     {return self;}
+
+    //...
 };
 
 //demo
 int main()
 {
-    cout<<"enter count of bytes:";
-    size_t f;
-    cin>>f;
+        cout<<"enter count of bytes:";
+        size_t f;
+        cin>>f;
+    signal steps(f);
 
     while(true)
     {
-        cout<<"enter signal divisor:";
-        size_t i;
-        cin>>i;
-        signal steps(f,i);
+            cout<<"enter signal divisor:";
+            size_t i;
+            cin>>i;
+        steps.generate(i);
         steps.out();
     }
 
+    steps.~signal();
     return 0;
 }
